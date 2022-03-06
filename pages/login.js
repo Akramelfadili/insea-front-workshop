51edf49b988d33fe4f3269bc1b53bcc7eba874cc
Akram <fadili261998@gmail.com>
2022-03-06 13:16:47 +0100

    base login component

---
+++ pages/login.js
@@ -0,0 +1,6 @@
+import AuthLayout from '../components/common/Layout/AuthLayout.js'
+export default function LoginPage() {
+    return ( <
+        AuthLayout > <h1> Login Page </h1></AuthLayout >
+    )
+}
